72128306bc1fe71556d120a41fea0a46de1c57e3
erman15 <ermansibarani9@gmail.com>
2023-02-04 10:36:45 +0700

    app listen port

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  // await app.listen(3000);
+  await app.listen(3000, function () {
+    console.log(
+      'Express server listening on port %d in %s mode',
+      this.address().port,
+    );
+  });
 }
 bootstrap();
